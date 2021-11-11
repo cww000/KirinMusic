6094ed424d8306565a1a4711b6b720157156149a
cww000 <1460964870@qq.com>
2021-11-11 12:20:40 +0800

    ad the agreement

--- decode.h
+++ decode.h
@@ -1,3 +1,23 @@
+/*
+* author: 程炆炆，聂海艳，李纯林
+* email：1460964870@qq.com 1933544851@qq.com 2742731130@qq.com
+* time:2021.10
+
+* Copyright (C) <2021>  <Wenwen Cheng,Haiyan Nie,Chunlin Li>
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef DECODE_H
 #define DECODE_H
 
